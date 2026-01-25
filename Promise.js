8bce7a209669067b09820b7ac7e0dc6b6688fdd7
Deepak Sharma <deepssharma0312@gmail.com>
2026-01-25 23:45:58 +0000

    Promises

---
+++ Promise.js
@@ -0,0 +1,65 @@
+const { rejects } = require("assert");
+const { error, Console } = require("console");
+const { resolve } = require("path");
+
+// let myPromise = new Promise((resolve, reject) => {
+//     console.log("Promise created, async task started...");
+
+//     //step 2: Async task (2 Seconds delay)
+//     setTimeout(() => {
+//         let success = true;
+//         if (success) {
+//             resolve("Task completed successfully!") //Step 3 : Resolve called
+//         } else {
+//             reject("Task failed")   //Step 3 : Reject called
+//         }
+//     }, 2000)
+// });
+
+
+// //Step 4 : Attach then() and catch() to handle result
+// myPromise
+//     .then(result => {
+//         console.log("Then:", result)
+//     })
+//     .catch(error => {
+//         console.log("Catch:", error)
+//     }
+//     )
+
+// console.log("This log runs BEFORE promise is resolved or rejected.");
+
+
+
+
+
+
+
+let myPromise2 = new Promise((resolve, reject) => {
+    console.log("Promise created") //Promise created
+
+    //Async task 
+    setTimeout(() => {
+        let value = false;
+        if (value) {
+            resolve("Task completed successfully!") //Resolve call
+        }
+        else {
+            reject("Task Rejected") //Reject call
+        }
+    }, 2000)
+})
+
+myPromise2
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+console.log("This is the shortest task to complete");
+
+
+
+
